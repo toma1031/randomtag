0b4d21d3edeb3efcb36f9994342d23466adfbe92
akinori.toma <ignitoma_1031@yahoo.co.jp>
2020-07-09 13:23:31 +0000

    fix SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       # render template: "users/index"
       redirect_to top_url
     else
-      flash.now[:danger] = 'Failed log'
+      flash.now[:danger] = 'Failed login'
       render :new
     end
   end
